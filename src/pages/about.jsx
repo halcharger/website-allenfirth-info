730e5d9350294b5f60829721bdb298897c3d4e2e
Allen Firth <allen@codestream.co.za>
2023-03-16 08:08:38 +0200

    tweak 001

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -48,7 +48,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Allen Firth. I live in Durban, South Africa.
+            I’m Allen Firth.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>I am an experienced full-stack software developer and architect. I studied correspondence through the University of South Africa
